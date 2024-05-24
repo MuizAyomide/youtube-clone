cc8342f77ece3bfce6f1ccdda1287ca1c75bd815
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-24 23:47:51 +0100

    commit

--- src/Data.js
+++ src/Data.js
@@ -1,13 +1,11 @@
-export const API_KEY = 'AIzaSyDu0IYKTmZnFe56Kw2s7EYCn204CyWe7VQ';
+export const API_KEY = "AIzaSyDu0IYKTmZnFe56Kw2s7EYCn204CyWe7VQ";
 
 export const value_converter = (value) => {
-    if (value >= 1000000){
-        return Math.floor(value/1000000)+'M';
-    } 
-    else if (value>=10000){
-        return  Math.floor(value/1000)+'K';
-    }
-    else{
-        return value;
-    }
-}
+  if (value >= 1000000) {
+    return Math.floor(value / 1000000) + "M";
+  } else if (value >= 10000) {
+    return Math.floor(value / 1000) + "K";
+  } else {
+    return value;
+  }
+};
