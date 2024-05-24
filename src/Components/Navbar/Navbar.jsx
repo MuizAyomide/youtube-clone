37405adf018bdc0ae599d66ebd4b87a4d8580aed
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-24 23:48:36 +0100

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,31 +9,34 @@ import notification_icon from "../../assets/notification.png";
 import profile_icon from "../../assets/jack.png";
 import { Link } from "react-router-dom";
 
-
-
-const Navbar = ({setSidebar}) => {
+const Navbar = ({ setSidebar }) => {
   return (
     <nav className="flex-div">
       <div className="nav-left flex-div">
-        <img className="menu-icon" onClick={()=>setSidebar(prev=>prev===false?true:false)} src={menu_icon} alt="" />
-      <Link to='/'><img className="logo" src={logo} alt="" /></Link>  
+        <img
+          className="menu-icon"
+          onClick={() => setSidebar((prev) => (prev === false ? true : false))}
+          src={menu_icon}
+          alt=""
+        />
+        <Link to="/">
+          <img className="logo" src={logo} alt="" />
+        </Link>
       </div>
 
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">
-        <input type="text" placeholder="Search" />
-        <img src={search_icon} alt="" />
+          <input type="text" placeholder="Search" />
+          <img src={search_icon} alt="" />
         </div>
       </div>
 
-<div className="nav-right flex-div">
-    <img src={upload_icon} alt="" />
-    <img src={more_icon} alt="" />
-    <img src={notification_icon} alt="" />
-    <img className="user-icon" src={profile_icon} alt="" />
-
-</div>
-
+      <div className="nav-right flex-div">
+        <img src={upload_icon} alt="" />
+        <img src={more_icon} alt="" />
+        <img src={notification_icon} alt="" />
+        <img className="user-icon" src={profile_icon} alt="" />
+      </div>
     </nav>
   );
 };
