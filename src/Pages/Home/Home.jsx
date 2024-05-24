c4db579c0d8979657db77ee672579b068a2809c6
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-24 23:49:20 +0100

    commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,6 @@ import Sidebar from "../../Components/Sidebar/Sidebar";
 import Feed from "../../Components/Feed/Feed";
 
 const Home = ({ sidebar }) => {
-
   const [category, setCategory] = useState(0);
 
   return (
