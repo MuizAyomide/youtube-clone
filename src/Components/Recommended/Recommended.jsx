67b9fab8a48171dd4eecb98443b0e17380e98ea5
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-10 19:29:01 +0100

    commit

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -9,6 +9,7 @@ import thumbnail6 from "../../assets/thumbnail6.png";
 import thumbnail7 from "../../assets/thumbnail7.png";
 import thumbnail8 from "../../assets/thumbnail8.png";
 import { API_KEY, value_converter } from "../../Data";
+
 import { Link } from "react-router-dom";
 
 const Recommended = ({ categoryId }) => {
